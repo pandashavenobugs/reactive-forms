85a363d4946d01a7fa8d8083d7f2bfaa926cff46
berat <cedinckan@gmail.com>
2020-12-19 22:03:20 +0300

    dynamic forms with array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 // initializing the system of form group
-import {FormBuilder,FormGroup} from "@angular/forms";
+import {FormBuilder,FormGroup,FormArray} from "@angular/forms";
 import {Subscription} from "rxjs"
 
 @Component({
@@ -13,19 +13,45 @@ export class AppComponent implements OnInit {
   formSubs:Subscription;
 
 
-  myform:FormGroup; // if we want to use reactive form, we have to initialize the form system
+  myForm:FormGroup; // if we want to use reactive form, we have to initialize the form system
 
   constructor(private formBuilder:FormBuilder){}
   ngOnInit():void{
-    this.myform= this.formBuilder.group({
+    // this.myform= this.formBuilder.group({
+    //   email:'',
+    //   message:'',
+    //   career:'',
+    // })
+    // this.formSubs=this.myform.valueChanges.
+    // subscribe(changes=>{
+    //   console.log(changes);
+    // })
+
+    // we can use formGroups in other formGroups
+    this.myForm = this.formBuilder.group({
       email:'',
-      message:'',
-      career:'',
+      phones:this.formBuilder.array([])
+    });
+    console.log(this.phoneForms);
+  }
+
+  get phoneForms(){
+    // we use phones property as a formArray so
+    // phones property is collect of formGroups
+    return this.myForm.get('phones') as FormArray;
+  }
+
+  addPhone(){
+    const phone = this.formBuilder.group({
+      area:[],
+      prefix:[],
+      line:[]
     })
-    this.formSubs=this.myform.valueChanges.
-    subscribe(changes=>{
-      console.log(changes);
-    })
+    this.phoneForms.push(phone);
+  }
+
+  deletePhone(i){
+    this.phoneForms.removeAt(i);
   }
 
 
