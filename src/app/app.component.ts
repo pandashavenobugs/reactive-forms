4f3f5d1ada399d8debff4a986b7f76ed02c8e763
berat <cedinckan@gmail.com>
2020-12-19 17:10:52 +0300

    basic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+// initializing the system of form group
+import {FormBuilder,FormGroup} from "@angular/forms";
+import {Subscription} from "rxjs"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms';
+  formSubs:Subscription;
+
+
+  myform:FormGroup; // if we want to use reactive form, we have to initialize the form system
+
+  constructor(private formBuilder:FormBuilder){}
+  ngOnInit():void{
+    this.myform= this.formBuilder.group({
+      email:'',
+      message:'',
+      career:'',
+    })
+    this.formSubs=this.myform.valueChanges.
+    subscribe(changes=>{
+      console.log(changes);
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {ReactiveFormsModule} from "@angular/forms"
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatChipsModule} from "@angular/material/chips"
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
